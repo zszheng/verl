53f16d61c8493172acf0fa8c7ea23a1c0d0b8bcd
zhengzhisheng@bilibili.com <zhengzhisheng@bilibili.com>
2025-04-12 00:09:04 +0800

    v100 and a10

--- examples/grpo_trainer/run_qwen2_5_vl-7b-v100.sh
+++ examples/grpo_trainer/run_qwen2_5_vl-7b-v100.sh
@@ -22,7 +22,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-7B-Instruct \
     actor_rollout_ref.model.model_dtype=float16 \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=$BATCH_SIZE \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
