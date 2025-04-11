296a384524941e2bbacc32b25cbc639cb1620c7d
spring <zszheng08@163.com>
2025-04-11 22:50:25 +0800

    Update run_qwen2_5_vl-7b-v100.sh

--- examples/grpo_trainer/run_qwen2_5_vl-7b-v100.sh
+++ examples/grpo_trainer/run_qwen2_5_vl-7b-v100.sh
@@ -2,25 +2,27 @@ python3 ~/code/verl/examples/data_preprocess/geo3k.py --local_dir ~/data/geo3k
 
 set -x
 ENGINE=${1:-vllm}
-#export VLLM_ATTENTION_BACKEND=XFORMERS
+export VLLM_ATTENTION_BACKEND=XFORMERS
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True,max_split_size_mb:64"
 
-QWEN=Qwen/Qwen2.5-VL-7B-Instruct
-#QWEN=/DATA/jupyter/personal/.cache/modelscope/Qwen/Qwen2.5-VL-3B-Instruct
+NODES=8
+BATCH_SIZE=$((NODES * 4))
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/data/geo3k/train.parquet \
     data.val_files=$HOME/data/geo3k/test.parquet \
-    data.train_batch_size=24 \
+    data.train_batch_size=$BATCH_SIZE \
     data.max_prompt_length=1024 \
-    data.max_response_length=256 \
+    data.max_response_length=2048 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.image_key=images \
-    actor_rollout_ref.model.path=$QWEN \
+    actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-7B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.model.use_remove_padding=False \
-    actor_rollout_ref.actor.ppo_mini_batch_size=24 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=$BATCH_SIZE \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.01 \
@@ -29,15 +31,17 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.actor.fsdp_config.fsdp_size=$BATCH_SIZE \
+    actor_rollout_ref.actor.fsdp_config.model_dtype=float16 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
+    actor_rollout_ref.rollout.max_num_batched_tokens=1280 \
     actor_rollout_ref.rollout.name=$ENGINE \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.free_cache_engine=False \
     actor_rollout_ref.rollout.n=1 \
-    actor_rollout_ref.rollout.dtype=half \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
@@ -46,7 +50,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.project_name='verl_grpo_example_geo3k' \
     trainer.experiment_name='qwen2_5_vl_7b_function_rm' \
     trainer.n_gpus_per_node=4 \
-    trainer.nnodes=6 \
+    trainer.nnodes=$NODES \
     trainer.save_freq=-1 \
     trainer.test_freq=5 \
     trainer.total_epochs=15 $@
