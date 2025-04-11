50788badcd5a2bc794ab0929d8208d4ac852abf4
spring <zszheng08@163.com>
2025-04-11 12:09:38 +0800

    Update run_qwen2_5_vl-7b-v100.sh

--- examples/grpo_trainer/run_qwen2_5_vl-7b-v100.sh
+++ examples/grpo_trainer/run_qwen2_5_vl-7b-v100.sh
@@ -1,17 +1,17 @@
-python3 ../data_preprocess/geo3k.py --local_dir ~/data/geo3k
+python3 ~/code/verl/examples/data_preprocess/geo3k.py --local_dir ~/data/geo3k
 
 set -x
 ENGINE=${1:-vllm}
 #export VLLM_ATTENTION_BACKEND=XFORMERS
 
 QWEN=Qwen/Qwen2.5-VL-7B-Instruct
-QWEN=/DATA/jupyter/personal/.cache/modelscope/Qwen/Qwen2.5-VL-3B-Instruct
+#QWEN=/DATA/jupyter/personal/.cache/modelscope/Qwen/Qwen2.5-VL-3B-Instruct
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/data/geo3k/train.parquet \
     data.val_files=$HOME/data/geo3k/test.parquet \
-    data.train_batch_size=20 \
+    data.train_batch_size=24 \
     data.max_prompt_length=1024 \
     data.max_response_length=256 \
     data.filter_overlong_prompts=True \
@@ -20,7 +20,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=$QWEN \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=False \
-    actor_rollout_ref.actor.ppo_mini_batch_size=20 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=24 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.01 \
@@ -46,7 +46,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.project_name='verl_grpo_example_geo3k' \
     trainer.experiment_name='qwen2_5_vl_7b_function_rm' \
     trainer.n_gpus_per_node=4 \
-    trainer.nnodes=5 \
+    trainer.nnodes=6 \
     trainer.save_freq=-1 \
     trainer.test_freq=5 \
     trainer.total_epochs=15 $@
