e763c7c0b507b94386a133137e6b7afc611900d7
zhengzhisheng@bilibili.com <zhengzhisheng@bilibili.com>
2025-04-15 17:06:47 +0800

    v100 and a10

--- examples/grpo_trainer/run_qwen2_5_vl-7b-a10.sh
+++ examples/grpo_trainer/run_qwen2_5_vl-7b-a10.sh
@@ -36,7 +36,6 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.fsdp_size=$BATCH_SIZE \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
-    actor_rollout_ref.rollout.max_num_batched_tokens=1280 \
     actor_rollout_ref.rollout.name=$ENGINE \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -983,8 +983,8 @@ class RewardModelWorker(Worker):
             warnings.simplefilter("ignore")
             setattr(model_config, 'classifier_dropout', 0.)
             reward_module = AutoModelForTokenClassification.from_pretrained(pretrained_model_name_or_path=local_path,
+                                                                            config=model_config,
                                                                             torch_dtype=torch.bfloat16,
-                                                                            config=actor_model_config,
                                                                             attn_implementation='flash_attention_2',
                                                                             trust_remote_code=trust_remote_code)
 
