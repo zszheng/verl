508d7def51fd9b216124601e48f536ff4c9ce607
zhengzhisheng@bilibili.com <zhengzhisheng@bilibili.com>
2025-04-16 19:15:49 +0800

    v100 and a10

--- examples/grpo_trainer/run_qwen2_5_vl-7b-a10.sh
+++ examples/grpo_trainer/run_qwen2_5_vl-7b-a10.sh
@@ -9,6 +9,8 @@ export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True,max_split_size_mb:64"
 NODES=2
 BATCH_SIZE=$((NODES * 4))
 
+#actor_rollout_ref.model.model_dtype=bfloat16 \
+
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/data/geo3k/train.parquet \
@@ -22,7 +24,6 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-7B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.model.model_dtype=bfloat16 \
     actor_rollout_ref.model.attn_impl=flash_attention_2 \
     actor_rollout_ref.actor.ppo_mini_batch_size=$BATCH_SIZE \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
