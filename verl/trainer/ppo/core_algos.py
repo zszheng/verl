46c8265bb5bf750431b1c8d033591f524fb316b4
zhengzhisheng@bilibili.com <zhengzhisheng@bilibili.com>
2025-04-17 12:48:23 +0800

    v100 and a10

--- verl/trainer/ppo/core_algos.py
+++ verl/trainer/ppo/core_algos.py
@@ -473,17 +473,24 @@ def kl_penalty(logprob: torch.FloatTensor, ref_logprob: torch.FloatTensor, kl_pe
         kl = ref_logprob - logprob
         ratio = torch.exp(kl)
         kld = (ratio - kl - 1).contiguous()
-        return torch.clamp(kld, min=-10, max=10)
+        return torch.clamp(kl, min=-5, max=5)
 
-    if kl_penalty == 'low_val_kl_fixed':
+    if kl_penalty == 'low_var_kl_fixed0':
+        kl = ref_logprob - logprob
+        ratio = torch.exp(kl)
+        kld = (ratio - kl - 1).contiguous()
+        return torch.clamp(kl, min=-5, max=5)
+
+    if kl_penalty == 'low_var_kl_fixed':
         kl = ref_logprob - logprob
         # 硬截断 kl 的范围（防止 exp 溢出）
-        kl_clamped = torch.clamp(kl, min=-15, max=15)
+        kl_clamped = torch.clamp(kl, min=-5, max=5)
         ratio = torch.exp(kl_clamped)
         # 计算 kld（基于截断后的 kl）
         kld = ratio - kl_clamped - 1
         # 最终输出截断（保留原始设计）
         return torch.clamp(kld, min=-10, max=10)
+
 
     if kl_penalty == "full":
         # so, here logprob and ref_logprob should contain the logits for every token in vocabulary
